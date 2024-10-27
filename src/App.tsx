63b524c66fdb6a19c156ca0783001e5db8681c9f
unkuseni <amudalatadeoye@gmail.com>
2024-10-27 01:21:30 +0100

    changed to react

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import { createSignal, createEffect } from "solid-js";
-import Hero from "./components/hero/hero";
-import Loader from "./components/loader/loader";
-import DefaultNav from "./components/nav/nav";
-import Drawer from "./components/drawers/drawer";
-function App() {
-	return (
-		<>
-			<div class={`flex flex-col sm:flex-row`}>
-				<Hero />
-        <Drawer num={1} text='wtf'/>
-        <Drawer num={2} text='yea' />
-        <Drawer num={3} text='dnd' />
-        <Drawer num={4} text='lol' />
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-			</div>
-		</>
-	);
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
